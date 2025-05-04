08fcafab4f2f29533d03304c511f6c3751f3d823
gustavo <gust.dovale@gmail.com>
2025-05-03 22:17:48 -0300

    Codigo generico para tela login

--- lib/pages/tela_login.dart
+++ lib/pages/tela_login.dart
@@ -5,6 +5,32 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Sistema do aplicativo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tela de Login'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Bem-vindo ao sistema!',
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Navegar para a tela inicial
+                  Navigator.pushReplacementNamed(context, '/home');
+                },
+                child: const Text('Entrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
